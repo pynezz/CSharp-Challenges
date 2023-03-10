194ef6f9df5c3a34cca9a7bb1de04ff2ba299500
pynezz <kadam@pm.me>
2023-03-10 15:25:27 +0100

    IP-validation

---
+++ IPValidation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Solution
+{
+    class Kata
+    {
+        public static bool IsValidIP(string input)
+        {
+            return input.Split('.').Length == 4 && input.Split('.').All(part => byte.TryParse(part, out var value) && value.ToString() == part && value >= 0 && value <= 255);
+        }
+    }
+}
